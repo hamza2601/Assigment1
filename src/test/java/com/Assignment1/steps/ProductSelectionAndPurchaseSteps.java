8e7358b3bee35b54b692787a064bac102f111e12
hamza2601 <hamza.ali@emumba.com>
2022-02-14 18:41:13 +0500

    feature file name changed

--- src/test/java/com/Assignment1/steps/ProductSelectionAndPurchaseSteps.java
+++ src/test/java/com/Assignment1/steps/ProductSelectionAndPurchaseSteps.java
@@ -1,36 +1,18 @@
 package com.Assignment1.steps;
 
-import BuyMoisturizer.Tasks.buyMoisturizer;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import homepage.questions.homepage.questions;
 import BuySunscreen.actions.NavigateTo;
-import homepage.tasks.homepage.tasks;
-import BuySunscreen.Tasks.buyCheapestSunscreen;
 
-import net.serenitybdd.screenplay.conditions.Check;
-
-import java.util.ArrayList;
-import java.util.Objects;
-
-import static BuyMoisturizer.UserInterface.Targets_Moisturizers.ALMOND_MOISTURIZERS;
-import static BuyMoisturizer.UserInterface.Targets_Moisturizers.ALOE_MOISTURIZERS;
-import static BuyMoisturizer.actions.buyCheapestMoisturizer.*;
 import static BuyMoisturizer.actions.moisturizerSelection.undertake_Moisturizer_Selection_And_Purchase;
-import static BuySunscreen.UserInterface.Targets.SUNSCREEN_SPF30;
-import static BuySunscreen.UserInterface.Targets.SUNSCREEN_SPF50;
 import static BuySunscreen.actions.SunscreenSelection.undertake_Sunscreen_Selection_And_Purchase;
-import static BuySunscreen.actions.buyingCheapestSunscreen.*;
 import static BuySunscreen.actions.intExtractionFromString.extractNumerals;
 
-import static checkoutCart.Actions.FieldFillingIn.fillInFields;
-import static checkoutCart.Actions.checkoutActions.*;
-import static checkoutCart.Actions.checkoutActionsMoisturizers.*;
-import static checkoutCart.Tasks.tasksCart.clickOnPayWithCard;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
-public class TempCheckSteps {
+public class ProductSelectionAndPurchaseSteps {
     String temp;
     @Given("the actor is on the homepage")
     public void actor_is_on_homepage()
@@ -53,7 +35,7 @@ public class TempCheckSteps {
 
 
        int temperature =  extractNumerals(temp);
-       temperature = 9;
+
 
       if(temperature >34)
       {
